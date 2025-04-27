db4b506085fe9221d6b2724136456bd741653df3
Suraj Telrandhe <suraj.telrandhe16@gmail.com>
2025-04-27 19:18:10 +0530

    feat: Add sale functionality to products with is_sale and sale_price fields

--- store/models.py
+++ store/models.py
@@ -29,6 +29,10 @@ class Product(models.Model):
     Category = models.ForeignKey(Category, on_delete=models.CASCADE,default=1)
     description = models.CharField(max_length=250, default='',blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/', height_field=None, width_field=None, max_length=None)
+    # add sale stuff
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=9)
+    
 
     def __str__(self):
         return self.name
